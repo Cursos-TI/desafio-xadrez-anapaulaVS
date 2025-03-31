1925f2aa4041cf7c1bfed73d0c95697eac6d7c32
anapaulaVS <valezeanapaula123@gmail.com>
2025-03-31 20:53:03 +0000

    Implementação de Movimentação da Torre

--- 02 AVENTUREIRO - Desafio Xadrez/02desafio.c
+++ 02 AVENTUREIRO - Desafio Xadrez/02desafio.c
@@ -4,6 +4,14 @@ int main() {
 
     printf("DESAFIO XADREZ - AVENTUREIRO! \n");
 
+     // Implementação de Movimentação da Torre
+
+     printf("Movimentação da Torre: \n");
+     int torreMovimento = 5;
+     for (int i = 0; i < torreMovimento; i++) {
+         printf("Direita \n");
+     }
+
      
 
     return 0;
