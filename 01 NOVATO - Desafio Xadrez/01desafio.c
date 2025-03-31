1f8a2350c068a110d96ce1eb636655d8193ab24b
anapaulaVS <valezeanapaula123@gmail.com>
2025-03-31 20:56:25 +0000

    FINALIZADO

--- 01 NOVATO - Desafio Xadrez/01desafio.c
+++ 01 NOVATO - Desafio Xadrez/01desafio.c
@@ -4,15 +4,15 @@ int main() {
 
     printf("DESAFIO XADREZ - NOVATO! \n");
 
-     // Implementação de Movimentação da Torre
+    // Implementação de Movimentação da Torre
 
-     printf("Movimentação da Torre: \n");
-     int torreMovimento = 5;
-     for (int i = 0; i < torreMovimento; i++) {
+    printf("Movimentação da Torre: \n");
+    int torreMovimento = 5;
+    for (int i = 0; i < torreMovimento; i++) {
          printf("Direita \n");
-     }
+    }
 
-     // Implementação de Movimentação do Bispo
+    // Implementação de Movimentação do Bispo
 
     printf("\nMovimentação do Bispo: \n");
     int bispoMovimento = 5;
@@ -22,15 +22,15 @@ int main() {
         i++;
     }
 
-     // Implementação de Movimentação da Rainha
+    // Implementação de Movimentação da Rainha
 
-     printf("\nMovimentação da Rainha: \n");
-     int rainhaMovimento = 8;
-     int j = 0;
-     do {
+    printf("\nMovimentação da Rainha: \n");
+    int rainhaMovimento = 8;
+    int j = 0;
+    do {
          printf("Esquerda \n");
          j++;
-     } while (j < rainhaMovimento);
+    } while (j < rainhaMovimento);
 
 
     return 0;
