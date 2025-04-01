2ec1f9e1530e51f41c8b792c71799e0cb9a47dd7
anapaulaVS <valezeanapaula123@gmail.com>
2025-04-01 09:28:17 +0000

    Implementação de Movimentação do Cavalo

--- 03 MESTRE - Desafio Xadrez/03desafio.c
+++ 03 MESTRE - Desafio Xadrez/03desafio.c
@@ -48,21 +48,34 @@ int main() {
     int torreMovimentoTotal = 5;
     torreMovimento(torreMovimentoTotal);
 
-     // Implementação de Movimentação do Bispo
+    // Implementação de Movimentação do Bispo
 
-     printf("\nMovimentação do Bispo:\n");
-     int bispoMovimentoTotal = 5;
-     bispoMovimento(bispoMovimentoTotal);
-     printf("\nMovimentação do Bispo com Loops Aninhados:\n");
-     int bispoMovimentoVertical = 2;
-     int bispoMovimentoHorizontal = 3;
-     bispoMovimentoAninhado(bispoMovimentoVertical, bispoMovimentoHorizontal);
+    printf("\nMovimentação do Bispo:\n");
+    int bispoMovimentoTotal = 5;
+    bispoMovimento(bispoMovimentoTotal);
+    printf("\nMovimentação do Bispo com Loops Aninhados:\n");
+    int bispoMovimentoVertical = 2;
+    int bispoMovimentoHorizontal = 3;
+    bispoMovimentoAninhado(bispoMovimentoVertical, bispoMovimentoHorizontal);
 
-     // Implementação de Movimentação da Rainha
+    // Implementação de Movimentação da Rainha
 
     printf("\nMovimentação da Rainha:\n");
     int rainhaMovimentoTotal = 8;
     rainhaMovimento(rainhaMovimentoTotal);
+
+    // Implementação de Movimentação do Cavalo
+
+    printf("\n\nMovimentação do Cavalo:\n");
+    int cavaloMovimentoVertical = 2;
+    int cavaloMovimentoHorizontal = 1;
+    for (int i = 0; i < cavaloMovimentoVertical; i++) {
+        printf("Cima\n");
+    }
+    for (int i = 0; i < cavaloMovimentoHorizontal; i++) {
+        printf("Direita\n");
+    }
+
 
     return 0;
     
