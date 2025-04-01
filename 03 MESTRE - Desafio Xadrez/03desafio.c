897be34c5a21e2e32bd4f657732f3581c71f8b2e
anapaulaVS <valezeanapaula123@gmail.com>
2025-04-01 09:03:11 +0000

    Função recursiva para simular o movimento da Torre

--- 03 MESTRE - Desafio Xadrez/03desafio.c
+++ 03 MESTRE - Desafio Xadrez/03desafio.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+// Função recursiva para simular o movimento da Torre
+
+void torreMovimento(int movimento) {
+    if (movimento > 0) {
+        printf("Direita\n");
+        torreMovimento(movimento - 1);
+    }
+}
+
 int main() {
 
     printf("DESAFIO XADREZ - MESTRE! \n");
